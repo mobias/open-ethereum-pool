60a4b937a5a1d260708c9f61081702395af58a76
xenhim <33386927+xenhim@users.noreply.github.com>
2018-01-07 04:45:09 -0800

    Update handlers.go

--- proxy/handlers.go
+++ proxy/handlers.go
@@ -5,8 +5,8 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/techievee/open-ethereum-pool/rpc"
-	"github.com/techievee/open-ethereum-pool/util"
+	"github.com/xenhim/open-ethereum-pool/rpc"
+	"github.com/xenhim/open-ethereum-pool/util"
 )
 
 // Allow only lowercase hexadecimal with 0x prefix
