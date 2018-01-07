be6cc6262a6f198e4521b7957ec92193d29a140c
xenhim <33386927+xenhim@users.noreply.github.com>
2018-01-07 04:42:17 -0800

    Update main.go

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 
 	"github.com/yvasiyarov/gorelic"
 
-	"github.com/techievee/open-ethereum-pool/api"
-	"github.com/techievee/open-ethereum-pool/payouts"
-	"github.com/techievee/open-ethereum-pool/proxy"
-	"github.com/techievee/open-ethereum-pool/storage"
+	"github.com/xenhim/open-ethereum-pool/api"
+	"github.com/xenhim/open-ethereum-pool/payouts"
+	"github.com/xenhim/open-ethereum-pool/proxy"
+	"github.com/xenhim/open-ethereum-pool/storage"
 )
 
 var cfg proxy.Config
